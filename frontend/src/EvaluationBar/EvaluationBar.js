3b516a5b30c3e41ba33d2710feaa10e3ed2d0c6f
Morgan Stanley <morganpstanley@gmail.com>
2021-02-02 15:55:26 -0700

    I lied on the last commit. This is the commit that changes the evaluation bar depending on player color

--- frontend/src/EvaluationBar/EvaluationBar.js
+++ frontend/src/EvaluationBar/EvaluationBar.js
@@ -1,48 +1,78 @@
 import { useState, useEffect } from "react"
 import "./EvaluationBar.css"
 
-const EvaluationBar = ({currentPlayer, evaluation, mate}) => {
+const colors = ["antiquewhite", "black"]
 
-    const [vh, setVh] = useState(0);
+const EvaluationBar = ({evaluationObj, playerColor}) => {
+
+    let {evaluation, color, mate} = evaluationObj
+
+    const [vh, setVh] = useState(1);
     const [better, setBetter] = useState('w');
     const [displayNum, setDisplayNum] = useState(0.2)
+    const [bottomColor, setBottomColor] = useState("antiquewhite")
+    const [topColor, setTopColor] = useState("black")
 
     useEffect(() => {
-        
+        if (playerColor === "white") {
+            setBottomColor(colors[0])
+            setTopColor(colors[1])
+        } else {
+            setBottomColor(colors[1])
+            setTopColor(colors[0])
+        }
+    }, [playerColor])
+
+    useEffect(() => {
         //set eval bar height
-        const  evalHeight =  currentPlayer === 'w' ? 
+        let  evalHeight =  color === 'w' ? 
         50 - (evaluation  * 4) :
         50 + (evaluation  * 4);
-        
-        if (evalHeight > 100) {
-            setVh(98)
-        } else if (evalHeight < 0) {
-            setVh(2)
-        } else {
-            setVh(evalHeight)
-        }
 
         if (evaluation > 0) {
-            setBetter(currentPlayer)
-            setDisplayNum(evaluation.toFixed(2))
+            setBetter(color)
         } else {
-            setDisplayNum(Math.abs(evaluation).toFixed(2))
+            color === "w" ? setBetter("b") : setBetter("w")
         };
+        
+        
+        //leave at least 2% if checkmate is not found
+        if (evalHeight > 100) {
+            evalHeight = 98
+        }
+        if (evalHeight < 0) {
+            evalHeight = 2
+        }
 
-    }, [currentPlayer, evaluation, mate, displayNum]);
+        if (playerColor === "black") {
+            evalHeight = 100 - evalHeight
+        }
+
+        setVh(evalHeight)
+
+        setDisplayNum(evaluation?.toFixed(2))
+
+    }, [playerColor, color, evaluation]);
+
+    useEffect(() => {
+        if (mate !== 0) {
+        better === "w" ? setVh(100) : setVh(0)
+        }
+    }, [mate, better])
 
     const displayEval = () => {
         if (mate !== 0) {
             return "#" + mate
         } else {
-            return better === 'w'  ? `+${displayNum}` : `-${displayNum}`
+            const symbol = better === "w" ? "+" : "-"
+            return symbol + Math.abs(displayNum).toFixed(1)
         }
     }
         
     return (
-        <div id="evaluation-bar-holder">
+        <div id="evaluation-bar-holder" style={{backgroundColor: bottomColor}}>
             <p id="eval">{displayEval()} </p>
-            <div id="evaluation-bar" style={{height: `${vh}%`, transition: 'height 2s'}} />
+            <div id="evaluation-bar" style={{height: `${vh}%`, transition: 'height 2s', backgroundColor: topColor}} />
         </div>
     );
 }
