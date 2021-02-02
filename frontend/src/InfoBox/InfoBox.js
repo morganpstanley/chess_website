716f90f6f4e6c5db0cb70183699a3ded1447c5eb
Morgan Stanley <morganpstanley@gmail.com>
2021-02-02 15:55:53 -0700

    add annotated moves to infoBox

--- frontend/src/InfoBox/InfoBox.js
+++ frontend/src/InfoBox/InfoBox.js
@@ -7,29 +7,52 @@ import Options from "../Options/Options"
 
 import "./InfoBox.css"
 
-const InfoBox = ({chess, fen, changeOpponent, changePlayerColor}) => {
+const InfoBox = ({chess, fen, pgn, changeOpponent, changePlayerColor}) => {
 
     const [opening, setOpening] = useState()
     const [eco, setEco] = useState()
     const [comment, setComment] = useState()
+    const [annotated, setAnnotated] = useState()
     const [options, showOptions] = useState(false)
 
     useEffect(() => {
         let fenArray = fen.split(' ')
         let fenAgain = fenArray[0] + " " + fenArray[1] + " " + fenArray[2]
-        let posistion = json.find(element => element.fen === fenAgain)
+        let posistion = json.find(opening => opening.fen === fenAgain)
 
         if (posistion && posistion.name !== posistion) {
             setOpening(posistion.name)
             setEco(posistion.eco)
         }
 
-        if (posistion && posistion.comment && posistion.comment !== comment) {
+        if (posistion && posistion.comment) {
             setComment(posistion.comment)
         } else if (posistion === undefined && comment !== "") {
             setComment("")
         }
-    }, [chess, fen, opening, eco, comment]);    
+    }, [chess, fen, opening, eco, comment, annotated]);
+
+    useEffect(() => {
+        const turn = chess.turn() === "w" ? 2 : 1
+        setAnnotated(json.filter(opening => {
+            const openingsLength  = opening.moves.split(' ').length;
+            let pgnLength = pgn.split(' ').length
+
+            if (pgnLength === 1) {pgnLength = 0} // splitting an empty string by " " returns an empty array with a length of 1...
+
+            return opening.moves.includes(pgn)
+            && openingsLength - pgnLength === turn
+            && opening.comment
+        }))
+    }, [pgn, chess])
+    
+    const annotatedMoves = () => {
+        return annotated?.map(opening => {
+        const arr = opening.moves.split(' ')
+        const move = arr[arr.length - 1]
+        return <button key={opening.name} className="annotated-move">{move}</button>
+        })
+    }
 
     return (
         <div id="infobox">
@@ -48,7 +71,13 @@ const InfoBox = ({chess, fen, changeOpponent, changePlayerColor}) => {
                     changeOpponent={changeOpponent} 
                     changePlayerColor={changePlayerColor}/> 
                 : comment}
+                <br />
                 <img id="commentary-background-image" src={openBook} alt="open book" />
+            </div>
+
+            <div id="continuations">
+            <span id="continuations-title">Annotated Continuations:</span> <br />
+            {annotatedMoves()}
             </div>
 
         </div>
